476e0ed3b94877ec24b4ac7758f1b6cd8970fd74
MakotoNakai <macofandohr1812@gmail.com>
2023-02-24 01:08:00 +0900

    get the content of key and cerm from pem

--- update_secret.go
+++ update_secret.go
@@ -5,25 +5,29 @@ import (
 	"fmt"
 	"flag"
 	"context"
+	"encoding/pem"
+	"encoding/base64"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/util/retry"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/apimachinery/pkg/api/errors"
+	// "k8s.io/apimachinery/pkg/api/errors"
 
+	v1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	coreV1Types "k8s.io/client-go/kubernetes/typed/core/v1"
 )
+
 
 func getk8sSecretClient() coreV1Types.SecretInterface {
 
 	kc := os.Getenv("HOME") + "/.kube/config"
 	c, err := clientcmd.BuildConfigFromFlags("", kc)
 	if err != nil {
-		panic("config building failed:" + err.Error())
+		fmt.Printf("config building failed: %v\n", err.Error())
 	}
 	nc, err := kubernetes.NewForConfig(c) 
 	if err != nil {
-		panic("creating new config failed:" + err.Error())
+		fmt.Printf("creating new config failed: %v\n", err.Error())
 	}
 	sc := nc.CoreV1().Secrets("default")
 	return sc
@@ -36,15 +40,28 @@ func main() {
 	kp := flag.String("key-path", "./build/secrets/server_key.pem", "The path to your end-entity private key")
   flag.Parse()
 
-	cert, err := os.ReadFile(*cp)
+	cpem, err := os.ReadFile(*cp)
 	if err != nil {
-    	panic("failed to read certificate: " + err.Error())
-    } 
+    	fmt.Printf("failed to read certificate: %v\n", err.Error())
+  } 
 
-	key, err := os.ReadFile(*kp)
+	kpem, err := os.ReadFile(*kp)
 	if err != nil {
-    	panic("failed to parse private key: " + err.Error())
-    }
+			fmt.Printf("failed to parse private key: %v\n", err.Error())
+	}
+
+	cblock, _ := pem.Decode(cpem)
+	if cblock == nil || cblock.Type != "CERTIFICATE" {
+		fmt.Println("failed to decode PEM block containing certificate")
+	}
+
+	kblock, _ := pem.Decode(kpem)
+	if kblock == nil || kblock.Type != "PRIVATE KEY" {
+		fmt.Println("failed to decode PEM block containing private key")
+	}
+
+	cert := base64.StdEncoding.EncodeToString(cblock.Bytes)
+	key := base64.StdEncoding.EncodeToString(kblock.Bytes)
 
 	sc := getk8sSecretClient()
 
@@ -55,20 +72,35 @@ func main() {
 		s, getErr := sc.Get(context.TODO(), "tls-secret", metaV1.GetOptions{})
 
 		// If secret "tls-secret" is not found
-		if errors.IsNotFound(getErr) {
-			panic(fmt.Errorf("Failed to get latest version of secret tls-secret: %v", getErr))
+		if getErr != nil {
+			sd := make(map[string]string)
+			sd["tls.crt"] = cert
+			sd["tls.key"] = key
+
+			s_ := &v1.Secret{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: "tls-secret",
+					Namespace: "default",
+				},
+				StringData: sd,
+			}
+
+			_, createErr := sc.Create(context.TODO(), s_, metaV1.CreateOptions{})
+			if createErr != nil {
+				fmt.Printf("Update failed: %v\n", createErr)
+			}
+			return createErr
 		}
 
-		s.Data["tls.crt"] = []byte(cert)
-		s.Data["tls.key"] = []byte(key)
-		fmt.Println(s)
+		s.StringData["tls.crt"] = cert
+		s.StringData["tls.key"] = key
 
 		_, updateErr := sc.Update(context.TODO(), s, metaV1.UpdateOptions{})
 		return updateErr
 	})
 
 	if retryErr != nil {
-		panic(fmt.Errorf("Update failed: %v", retryErr))
+		fmt.Printf("Update failed: %v\n", retryErr)
 	}
 	fmt.Println("Secret tls-secret is successfully updated")
 
