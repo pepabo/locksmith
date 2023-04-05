1e59234fc98b11cd062b12dfd5e164efe3a3e25c
MakotoNakai <macofandohr1812@gmail.com>
2023-04-05 13:34:50 +0900

    [fix] update the commands for retrieving container ids

--- build/create_secret_server.sh
+++ build/create_secret_server.sh
@@ -1,6 +1,6 @@
 docker build ./ -t server_image -f dockerfile_server
 docker run --name server -it server_image:latest
-export SERVER_CONTAINER_ID=$(docker ps -a --filter name=server -q)
+export SERVER_CONTAINER_ID=$(docker ps -a | grep server_image:latest | awk '{print $1}')
 
 docker cp $SERVER_CONTAINER_ID:/opt/pki/RootCA/server_crt.pem secrets/server_crt.pem
 docker cp $SERVER_CONTAINER_ID:/opt/pki/RootCA/server_key.pem secrets/server_key.pem
